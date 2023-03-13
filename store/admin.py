900ce7b2ca938373de1c374761b9e0993eeea62d
Eyetu Kingsley <eyetukingsley330@gmail.com>
2023-03-13 23:28:28 +0100

    I have removed the weights model and integrated a fixed weight system. I have created fixtures for addons and categories.

--- store/admin.py
+++ store/admin.py
@@ -3,7 +3,6 @@ from .models import *
 
 # Register your models here.
 admin.site.register(Product) 
-admin.site.register(Weight)
 admin.site.register(Ingredient)
 admin.site.register(Addon)
 admin.site.register(Category)
--- store/models.py
+++ store/models.py
@@ -8,12 +8,18 @@ from fractions import Fraction
 
 # Create your models here.
 class Product(models.Model):
+    WEIGHT_CHOICES = (
+        ('28g', '28 Grams'),
+        ('1/2lb', '1/2 Pound'),
+        ('1/4lb', '1/4 Pound'),
+    )
+
     name = models.CharField(max_length=120)
     image = models.ImageField(upload_to="products/")
     short_description = models.CharField(max_length=160)
     description = models.TextField()
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
-    weights = models.ManyToManyField('Weight')
+    weights = models.CharField(max_length=10, choices=WEIGHT_CHOICES)
     price = models.FloatField()
     previous_price = models.FloatField()
     addons = models.ManyToManyField('Addon')
@@ -33,38 +39,6 @@ class Product(models.Model):
     class Meta:
         verbose_name = "Product"
         verbose_name_plural = "Products"
-
-
-class Weight(models.Model):
-    WEIGHT_CHOICES = (
-        ('g', 'Gram'),
-        ('oz', 'Ounce (28 grams)'),
-        ('lb', 'Pound (16 ounces)'),
-    )
-
-    quantity = models.DecimalField(max_digits=4, decimal_places=2)
-    unit = models.CharField(max_length=20, choices=WEIGHT_CHOICES)
-
-    def as_fraction(self):
-        fraction_form = Fraction(self.quantity).limit_denominator()
-        if fraction_form.denominator == 1:
-            the_fraction = fraction_form.numerator
-        elif fraction_form.numerator > fraction_form.denominator:
-            whole_num = int(fraction_form.numerator / fraction_form.denominator)
-            mixed_fraction = str(fraction_form.numerator % fraction_form.denominator) + "/" + str(fraction_form.denominator)
-            the_fraction = f"{whole_num} {mixed_fraction}"
-        else:
-            the_fraction = str(fraction_form.numerator) + "/" + str(fraction_form.denominator)
-            
-        return the_fraction
-    # Which of the following is the best way to represent the following data in an admin interface: '4 1/2lb', '4 1/2 lb' or any way you can come up with
-
-    def __str__(self):
-        return self.as_fraction() + self.unit
-
-    class Meta:
-        verbose_name = "Weight"
-        verbose_name_plural = "Weights"
 
 
 class Ingredient(models.Model):

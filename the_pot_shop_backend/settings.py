506cfcbd131684fd199c66d9e4cd49ead71a3d6d
Eyetu Kingsley <eyetukingsley330@gmail.com>
2023-03-26 12:33:38 +0100

    I have added django-cors-headers to allow my frontend call my api.

--- the_pot_shop_backend/settings.py
+++ the_pot_shop_backend/settings.py
@@ -39,12 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'authentication.apps.AuthenticationConfig',
     'store.apps.StoreConfig',
+    'corsheaders',
     'rest_framework',
     'django_filters',
     'django_archive'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -141,3 +143,10 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 # Settings for the django_archive module
 ARCHIVE_DIRECTORY = BASE_DIR / 'db-backups'
+
+# Settings for django-cors-headers
+# CORS_ALLOWED_ORIGINS = [
+#     'https://127.0.0.1:3000'
+# ]
+
+CORS_ALLOW_ALL_ORIGINS = True

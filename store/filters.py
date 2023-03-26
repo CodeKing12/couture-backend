9f0abac27b7201c8189fed5aeba26ad12f84bb6a
Eyetu Kingsley <eyetukingsley330@gmail.com>
2023-03-26 01:10:42 +0100

    I have added the functionality to specify multiple review stars to filter by (the frontend sends the minimum and maximum review stars chosen and the backend filters as such.

--- store/filters.py
+++ store/filters.py
@@ -1,6 +1,9 @@
 from .models import Product
 from django_filters import rest_framework as filters
 
+class NumberRangeFilter(filters.BaseInFilter, filters.NumberFilter):
+    pass
+
 class ProductFilter(filters.FilterSet):
     name = filters.CharFilter(field_name='name', lookup_expr='icontains')
     description = filters.CharFilter(field_name='short_description', lookup_expr='icontains')
@@ -8,8 +11,10 @@ class ProductFilter(filters.FilterSet):
     composition = filters.CharFilter(field_name='composition__name', lookup_expr='exact')
     min_price = filters.NumberFilter(field_name='price', lookup_expr='gte')
     max_price = filters.NumberFilter(field_name='price', lookup_expr='lte')
-    reviews = filters.NumberFilter(field_name="reviews_aggr", lookup_expr='contains')
+    reviews = filters.NumberFilter(field_name="reviews_aggr", lookup_expr='contains', label='Review Aggregate is')
+    min_reviews = filters.NumberFilter(field_name="reviews_aggr", lookup_expr='gte')
+    max_reviews = filters.NumberFilter(field_name="reviews_aggr", lookup_expr='lte')
 
     class Meta:
         model = Product
-        fields = ['name', 'description', 'category', 'composition', 'price', 'review']
+        fields = ['name', 'description', 'category', 'composition']
